debc8198fd209f8b09bf4a42a29a4e94aaea1f20
Aditya Desai <adityadesaii2476@gmail.com>
2026-07-04 19:49:44 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello from Docker 🚀</h1>"
+
+@app.route("/about")
+def about():
+    return "<h2>I built this application using Docker!</h2>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
